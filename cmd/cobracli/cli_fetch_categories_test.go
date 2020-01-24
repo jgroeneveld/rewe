8966ec4fc97871e59b68d046fcd332e6bc6d51e1
Jaap Groeneveld <moin@jgroeneveld.de>
2020-01-24 16:19:47 +0100

    add integration test for cobra cli

---
+++ cmd/cobracli/cli_fetch_categories_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestCLI_Categories(t *testing.T) {
+	server := newFixtureServer(t, "Butter", "search_butter.html")
+	defer server.Close()
+
+	output := bytes.NewBuffer(nil)
+
+	cmd := rootCommand(output)
+	cmd.SetArgs([]string{
+		"--base-url", server.URL,
+		"fetch-categories",
+		"Butter",
+		"--json",
+	})
+
+	err := cmd.Execute()
+	assert.NilError(t, err)
+
+	assert.Equal(t, output.String(), `{
+  "product": "Landliebe Butter 250g",
+  "categories": [
+    "/c/frische-kuehlung",
+    "/c/frische-kuehlung-eier-fett-molkereiprodukte",
+    "/c/frische-kuehlung-eier-fett-molkereiprodukte-margarine-butter-fett",
+    "/c/frische-kuehlung-eier-fett-molkereiprodukte-margarine-butter-fett-butter"
+  ]
+}
+`)
+}
